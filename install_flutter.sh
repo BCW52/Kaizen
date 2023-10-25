d800b4cea2cf4f048808008a6af13a9676b542e5
BCW52 <nahidislam420gamer@gmail.com>
2023-10-25 18:08:08 +0600

    27

--- install_flutter.sh
+++ install_flutter.sh
@@ -2,25 +2,25 @@
 
 # Define the installation directory
 FLUTTER_INSTALL_DIR="$HOME/development"
-FLUTTER_SDK_URL="https://flutter.dev/docs/get-started/install/linux"
-FLUTTER_TAR_URL="https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_2.5.0-stable.tar.xz"
-sudo apt-get install xz-utils
+
+# URL for the Flutter SDK tar.xz file
+FLUTTER_SDK_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.13.8-stable.tar.xz"
 
 # Install Flutter
 if ! command -v flutter &> /dev/null; then
   echo "Flutter not found. Installing..."
-  
+
   # Create the installation directory
   mkdir -p "$FLUTTER_INSTALL_DIR"
-  
+
   # Download and extract Flutter
   cd "$FLUTTER_INSTALL_DIR"
-  curl -O -L "$FLUTTER_TAR_URL"
-  tar -xvf "flutter_linux_3.13.8-stable.tar.xz"
-  
+  curl -o flutter.tar.xz -L "$FLUTTER_SDK_URL"
+  tar xf "flutter.tar.xz"
+
   # Add Flutter to the PATH for this session
-  export PATH="$PATH:$FLUTTER_INSTALL_DIR/flutter/bin"
-  
+  export PATH="$PATH:`pwd`/flutter/bin"
+
   echo "Flutter installation completed."
 else
   echo "Flutter is already installed."
@@ -28,13 +28,10 @@ fi
 
 # Check if Flutter is in the PATH for all future sessions
 if ! grep -q "flutter/bin" "$HOME/.bashrc"; then
-  echo 'export PATH="$PATH:'"$FLUTTER_INSTALL_DIR"'/flutter/bin"' >> "$HOME/.bashrc"
+  echo 'export PATH="$PATH:'"`pwd`/flutter/bin"' >> "$HOME/.bashrc"
   echo "PATH updated for all future sessions."
 fi
 
 # Print installation instructions
 echo "Please review the Flutter installation instructions at:"
-echo "$FLUTTER_SDK_URL"
-
-# Verify Flutter installation
-flutter --version
+echo "https://flutter.dev/docs/get-started/install/linux"
